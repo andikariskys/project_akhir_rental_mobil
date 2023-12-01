741917ca9f829428569acadd970be9597cdb27a4
andikariskys <andikarisky313@gmail.com>
2023-12-01 19:33:00 +0700

    make form input from user

--- Config.py
+++ Config.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Config():
     merk_mobil = open("merk.txt")
     merk = merk_mobil.readlines()
@@ -7,6 +9,51 @@ class Config():
     
     hrg_sewa_mobil = open("hrg_sewa.txt")
     hrg_sewa = hrg_sewa_mobil.readlines()
+    
+    def get_data_penyewaan():
+        indek_mobil = int(input("Pilihan mobil Anda? (1/2/3/...) \n => "))
+        
+        ulangi_bulan_ambil = True
+        while ulangi_bulan_ambil:
+            bulan_ambil = int(input("Masukkan bulan ambil mobil (1 - 12) \n => "))
+            if len(str(bulan_ambil)) > 2 or bulan_ambil > 12 or bulan_ambil < 1:
+                print("Bulan harus diantara 1 - 12")
+            elif bulan_ambil >= date.today().month:
+                print("Bulan pengambilan minimal bulan ini")
+            else:
+                ulangi_bulan_ambil = False
+        
+        ulangi_tanggal_ambil = True
+        while ulangi_tanggal_ambil:
+            tanggal_ambil = int(input("Masukkan tanggal ambil mobil (1 - 31) \n => "))
+            if len(str(tanggal_ambil)) > 2 or tanggal_ambil > 31 or tanggal_ambil < 1:
+                print("Tanggal harus diantara 1 - 31")
+            elif tanggal_ambil >= date.today().day:
+                print("Tanggal pengambilan minimal hari ini")
+            else:
+                ulangi_tanggal_ambil = False
+        
+        ulangi_bulan_kembali = True
+        while ulangi_bulan_kembali:
+            bulan_kembali = int(input("Masukkan bulan kembali mobil (1 - 12) \n => "))
+            if len(str(bulan_kembali)) > 2 or bulan_kembali > 12 or bulan_kembali < 1:
+                print("Bulan harus diantara 1 - 12")
+            elif bulan_kembali >= date.today().month:
+                print("Bulan pengembalian minimal bulan ini")
+            else:
+                ulangi_bulan_kembali = False
+        
+        ulangi_tanggal_kembali = True
+        while ulangi_tanggal_kembali:
+            tanggal_kembali = int(input("Masukkan tanggal kembali mobil (1 - 31) \n => "))
+            if len(str(tanggal_kembali)) > 2 or tanggal_kembali > 31 or tanggal_kembali < 1:
+                print("Tanggal harus diantara 1 - 31")
+            elif tanggal_kembali >= date.today().day:
+                print("Tanggal pengembalian minimal besok (1 hari)")
+            else:
+                ulangi_tanggal_kembali = False
+        
+        berkas_penyewaan = input("Apakah berkas penyewaan sudah lengkap? Cth. KTP/NPWP/SIM/... (Ya/Y atau Tidak/T) \n =>")
     
     def get_daftar_mobil():
         i = 0
@@ -29,5 +76,6 @@ class Config():
     merk_mobil.close()
     jml_kursi_mobil.close()
     hrg_sewa_mobil.close()
-
-Config.get_daftar_mobil()
+    
+# Config.get_daftar_mobil()
+Config.get_data_penyewaan()
