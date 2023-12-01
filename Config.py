aea2a5d161677eb1f76489c614005022cbc5438f
andikariskys <andikarisky313@gmail.com>
2023-12-01 21:54:01 +0700

    fix bugs

--- Config.py
+++ Config.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-class Config():
+class classConfig:
     merk_mobil = open("merk.txt")
     merk = merk_mobil.readlines()
     
@@ -10,15 +10,23 @@ class Config():
     hrg_sewa_mobil = open("hrg_sewa.txt")
     hrg_sewa = hrg_sewa_mobil.readlines()
     
-    def get_data_penyewaan():
-        indek_mobil = int(input("Pilihan mobil Anda? (1/2/3/...) \n => "))
+    indeks = 0
+    
+    def get_data_penyewaan(self):
+        ulangi_pilih_mobil = True
+        while ulangi_pilih_mobil:
+            pilih_mobil = int(input("Pilihan mobil Anda? (1/2/3/...) \n => "))
+            if pilih_mobil < 1 or pilih_mobil > self.indeks :
+                print("Pilih nomor kendaraan yang tepat")
+            else:
+                ulangi_pilih_mobil = False
         
         ulangi_bulan_ambil = True
         while ulangi_bulan_ambil:
             bulan_ambil = int(input("Masukkan bulan ambil mobil (1 - 12) \n => "))
             if len(str(bulan_ambil)) > 2 or bulan_ambil > 12 or bulan_ambil < 1:
                 print("Bulan harus diantara 1 - 12")
-            elif bulan_ambil >= date.today().month:
+            elif bulan_ambil < date.today().month:
                 print("Bulan pengambilan minimal bulan ini")
             else:
                 ulangi_bulan_ambil = False
@@ -28,7 +36,7 @@ class Config():
             tanggal_ambil = int(input("Masukkan tanggal ambil mobil (1 - 31) \n => "))
             if len(str(tanggal_ambil)) > 2 or tanggal_ambil > 31 or tanggal_ambil < 1:
                 print("Tanggal harus diantara 1 - 31")
-            elif tanggal_ambil >= date.today().day:
+            elif tanggal_ambil < date.today().day:
                 print("Tanggal pengambilan minimal hari ini")
             else:
                 ulangi_tanggal_ambil = False
@@ -38,7 +46,7 @@ class Config():
             bulan_kembali = int(input("Masukkan bulan kembali mobil (1 - 12) \n => "))
             if len(str(bulan_kembali)) > 2 or bulan_kembali > 12 or bulan_kembali < 1:
                 print("Bulan harus diantara 1 - 12")
-            elif bulan_kembali >= date.today().month:
+            elif bulan_kembali < bulan_ambil:
                 print("Bulan pengembalian minimal bulan ini")
             else:
                 ulangi_bulan_kembali = False
@@ -48,34 +56,33 @@ class Config():
             tanggal_kembali = int(input("Masukkan tanggal kembali mobil (1 - 31) \n => "))
             if len(str(tanggal_kembali)) > 2 or tanggal_kembali > 31 or tanggal_kembali < 1:
                 print("Tanggal harus diantara 1 - 31")
-            elif tanggal_kembali >= date.today().day:
+            elif tanggal_kembali <= tanggal_ambil:
                 print("Tanggal pengembalian minimal besok (1 hari)")
             else:
                 ulangi_tanggal_kembali = False
         
-        berkas_penyewaan = input("Apakah berkas penyewaan sudah lengkap? Cth. KTP/NPWP/SIM/... (Ya/Y atau Tidak/T) \n =>")
+        berkas_penyewaan = input("Apakah berkas penyewaan sudah lengkap? Cth. KTP/NPWP/SIM/... (Ya/Y atau Tidak/T) \n => ")
     
-    def get_daftar_mobil():
-        i = 0
-        
+    def get_daftar_mobil(self):
         print()
         print('   Daftar Harga Sewa Mobil Automatic/Manual Harian')
         print(f"| {'-'*3} | {'-'*10} | {'-'*18} | {'-'*11} |")
         print(f"| {'No' :<3} | {'Merk Mobil' :<10} | {'Jumlah Kursi' :<18} | {'Harga Sewa' :<11} |")
         print(f"| {'-'*3} | {'-'*10} | {'-'*18} | {'-'*11} |")
         
-        while i < len(Config.merk):
-            m_merk = str.replace(Config.merk[i], '\n', '')
-            m_jml_kursi = int(Config.jml_kursi[i])
-            m_hrg_sewa = str(int(Config.hrg_sewa[i])) + '.000'
+        while self.indeks < len(self.merk):
+            m_merk = str.replace(self.merk[self.indeks], '\n', '')
+            m_jml_kursi = int(self.jml_kursi[self.indeks])
+            m_hrg_sewa = str(int(self.hrg_sewa[self.indeks])) + '.000'
             
-            print(f"| {i+1 :<3} | {m_merk :<10} | {m_jml_kursi} / {m_jml_kursi + 1} {'Tanpa sopir' :<12} | Rp {m_hrg_sewa :<8} |")
+            print(f"| {self.indeks+1 :<3} | {m_merk :<10} | {m_jml_kursi} / {m_jml_kursi + 1} {'Tanpa sopir' :<12} | Rp {m_hrg_sewa :<8} |")
             
-            i +=1
+            self.indeks +=1
     
     merk_mobil.close()
     jml_kursi_mobil.close()
     hrg_sewa_mobil.close()
-    
-# Config.get_daftar_mobil()
-Config.get_data_penyewaan()
+
+cfg = classConfig()
+cfg.get_daftar_mobil()
+cfg.get_data_penyewaan()
